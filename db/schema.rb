9c0fd5ca056c98b510af88ab4714ac3d9bb17c84
Sam Potasznik <sampotasznik@gmail.com>
2011-11-13 20:18:50 -0500

    added users scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,21 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111108020006) do
+ActiveRecord::Schema.define(:version => 20111114010116) do
 
   create_table "feeds", :force => true do |t|
     t.string   "name"
     t.string   "url"
     t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.text     "afillation"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
